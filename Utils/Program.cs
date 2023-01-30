42cb38f14892e40355ed0f7972faeab4d89fbb21
charlie-vanabbema-student <112420966+charlie-vanabbema-student@users.noreply.github.com>
2023-01-30 19:06:39 +0000

    ASDasd

--- Utils/Program.cs
+++ Utils/Program.cs
@@ -1,11 +1,1 @@
-﻿using MyUtilities;
-
-Console.WriteLine(StringUtils.VowelsToLower("hEllo"));
-Console.WriteLine(StringUtils.VowelsToUpper("hEllo"));
-// Console.WriteLine(StringUtils.ToUpper("hEllo"));
-// Console.WriteLine(StringUtils.ToLower("hEllo"));
-Console.WriteLine(StringUtils.Extend("hEllo"));
-Console.WriteLine(StringUtils.Scramble("Precipitation"));
-
-// Console.WriteLine(StringUtils.ToUpper("tacocat"));
-// Console.WriteLine(StringUtils.CountVowels("tAcOcat"));
+﻿
